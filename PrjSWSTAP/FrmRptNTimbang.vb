4ab93bbc2d3cf879cb8372642baadf562980c96e
jbwp17 <bwaldo46@gmail.com>
2017-09-25 10:06:37 +0700

    RPT Penerimaan,numpang timbang

---
+++ PrjSWSTAP/FrmRptNTimbang.vb
@@ -0,0 +1,75 @@
+﻿Imports System.IO
+
+Imports DevExpress.XtraGrid.Views.Base
+Imports DevExpress.XtraGrid.Views.Grid
+Imports DevExpress.XtraGrid.Columns
+Imports DevExpress.XtraGrid.Views.BandedGrid
+Imports DevExpress.XtraEditors.Repository
+Imports DevExpress.Export
+Imports DevExpress.XtraPrinting
+
+''
+''.Oracle
+'Imports Devart.Common
+
+Imports Microsoft.VisualBasic
+Imports System
+Public Class FrmRptNTimbang
+    Private Sub SimpleButton5_Click(sender As Object, e As EventArgs) Handles SimpleButton5.Click
+        Me.Close()
+    End Sub
+
+    Private Sub SimpleButton1_Click(sender As Object, e As EventArgs) Handles SimpleButton1.Click
+        Loadview()
+    End Sub
+    Private Sub Loadview()
+        Dim tgl1 As String
+        Dim tgl2 As String
+
+
+        DateEdit1.Properties.Mask.Culture = New System.Globalization.CultureInfo("en-US")
+        DateEdit1.Properties.Mask.EditMask = "dd-MM-yyyy"
+        DateEdit1.Properties.Mask.UseMaskAsDisplayFormat = True
+        DateEdit1.Properties.CharacterCasing = CharacterCasing.Upper
+        tgl1 = "DATE_IN('" & DateEdit1.Text & "','dd-MM-yyyy')"
+
+        DateEdit2.Properties.Mask.Culture = New System.Globalization.CultureInfo("en-US")
+        DateEdit2.Properties.Mask.EditMask = "dd-MM-yyyy"
+        DateEdit2.Properties.Mask.UseMaskAsDisplayFormat = True
+        DateEdit2.Properties.CharacterCasing = CharacterCasing.Upper
+        tgl2 = "DATE_OUT('" & DateEdit2.Text & "','dd-MM-yyyy')"
+        If DateEdit1.Text <> "" = True Or DateEdit2.Text <> "" = True Then
+
+            SQL = "SELECT * FROM V_TICKET_FINISH WHERE DATE_IN BETWEEN " & tgl1 & " AND " & tgl2 & "" +
+           " ORDER BY INPUT_DATE DESC"
+
+        Else
+            SQL = "SELECT * FROM V_TICKET_FINISH ORDER BY DATE_IN"
+
+        End If
+        FILLGridView(SQL, GridControl2)
+
+
+    End Sub
+
+    Private Sub SimpleButton2_Click(sender As Object, e As EventArgs) Handles SimpleButton2.Click
+        'ex to xls
+        Dim myStream As Stream
+        Dim saveFileDialog1 As New SaveFileDialog()
+
+        saveFileDialog1.Filter = "Excel files (*.xls)|*.xls|All files (*.*)|*.*"
+        saveFileDialog1.FilterIndex = 1
+        saveFileDialog1.RestoreDirectory = True
+
+        If saveFileDialog1.ShowDialog() = DialogResult.OK Then
+            myStream = saveFileDialog1.OpenFile()
+            If (myStream IsNot Nothing) Then
+                Dim nama As String = saveFileDialog1.FileName
+                myStream.Close()
+                GridView2.ExportToXls(nama)
+                MsgBox("Export successfull!", MessageBoxIcon.Information, "Report Penjualan")
+            End If
+
+        End If
+    End Sub
+End Class
---
+++ PrjSWSTAP/FrmRptPenerimaan.designer.vb
@@ -0,0 +1,308 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmRptPenerimaan
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmRptPenerimaan))
+        Me.GridColumn1 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridControl2 = New DevExpress.XtraGrid.GridControl()
+        Me.GridView2 = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.LabelControl89 = New DevExpress.XtraEditors.LabelControl()
+        Me.PanelControl6 = New DevExpress.XtraEditors.PanelControl()
+        Me.SimpleButton1 = New DevExpress.XtraEditors.SimpleButton()
+        Me.SimpleButton5 = New DevExpress.XtraEditors.SimpleButton()
+        Me.SimpleButton3 = New DevExpress.XtraEditors.SimpleButton()
+        Me.Panel7 = New System.Windows.Forms.Panel()
+        Me.LabelControl2 = New DevExpress.XtraEditors.LabelControl()
+        Me.LabelControl1 = New DevExpress.XtraEditors.LabelControl()
+        Me.LabelControl80 = New DevExpress.XtraEditors.LabelControl()
+        Me.LabelControl81 = New DevExpress.XtraEditors.LabelControl()
+        Me.BunifuGradientPanel2 = New ns1.BunifuGradientPanel()
+        Me.ComboBoxEdit2 = New DevExpress.XtraEditors.ComboBoxEdit()
+        Me.ComboBoxEdit1 = New DevExpress.XtraEditors.ComboBoxEdit()
+        Me.DateEdit1 = New DevExpress.XtraEditors.DateEdit()
+        Me.DateEdit2 = New DevExpress.XtraEditors.DateEdit()
+        CType(Me.GridControl2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GridView2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PanelControl6, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PanelControl6.SuspendLayout()
+        Me.Panel7.SuspendLayout()
+        Me.BunifuGradientPanel2.SuspendLayout()
+        CType(Me.ComboBoxEdit2.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ComboBoxEdit1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DateEdit1.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DateEdit1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DateEdit2.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DateEdit2.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GridColumn1
+        '
+        Me.GridColumn1.Name = "GridColumn1"
+        Me.GridColumn1.Visible = True
+        Me.GridColumn1.VisibleIndex = 0
+        '
+        'GridControl2
+        '
+        Me.GridControl2.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.GridControl2.Location = New System.Drawing.Point(153, 184)
+        Me.GridControl2.MainView = Me.GridView2
+        Me.GridControl2.Name = "GridControl2"
+        Me.GridControl2.Size = New System.Drawing.Size(680, 309)
+        Me.GridControl2.TabIndex = 93
+        Me.GridControl2.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GridView2})
+        '
+        'GridView2
+        '
+        Me.GridView2.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.GridColumn1})
+        Me.GridView2.GridControl = Me.GridControl2
+        Me.GridView2.Name = "GridView2"
+        Me.GridView2.OptionsSelection.MultiSelect = True
+        Me.GridView2.OptionsSelection.ShowCheckBoxSelectorInColumnHeader = DevExpress.Utils.DefaultBoolean.[False]
+        Me.GridView2.OptionsSelection.ShowCheckBoxSelectorInGroupRow = DevExpress.Utils.DefaultBoolean.[True]
+        Me.GridView2.OptionsView.ShowGroupPanel = False
+        '
+        'LabelControl89
+        '
+        Me.LabelControl89.Appearance.Font = New System.Drawing.Font("Tahoma", 9.25!, System.Drawing.FontStyle.Bold)
+        Me.LabelControl89.Appearance.ForeColor = System.Drawing.Color.White
+        Me.LabelControl89.Appearance.Options.UseFont = True
+        Me.LabelControl89.Appearance.Options.UseForeColor = True
+        Me.LabelControl89.Location = New System.Drawing.Point(12, 15)
+        Me.LabelControl89.Name = "LabelControl89"
+        Me.LabelControl89.Size = New System.Drawing.Size(181, 14)
+        Me.LabelControl89.TabIndex = 0
+        Me.LabelControl89.Text = "WEIGHBRIDGE - PENERIMAAN"
+        '
+        'PanelControl6
+        '
+        Me.PanelControl6.Controls.Add(Me.SimpleButton1)
+        Me.PanelControl6.Controls.Add(Me.SimpleButton5)
+        Me.PanelControl6.Controls.Add(Me.SimpleButton3)
+        Me.PanelControl6.Dock = System.Windows.Forms.DockStyle.Top
+        Me.PanelControl6.Location = New System.Drawing.Point(153, 43)
+        Me.PanelControl6.Name = "PanelControl6"
+        Me.PanelControl6.Size = New System.Drawing.Size(680, 39)
+        Me.PanelControl6.TabIndex = 89
+        '
+        'SimpleButton1
+        '
+        Me.SimpleButton1.Appearance.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Bold)
+        Me.SimpleButton1.Appearance.ForeColor = System.Drawing.Color.Green
+        Me.SimpleButton1.Appearance.Options.UseFont = True
+        Me.SimpleButton1.Appearance.Options.UseForeColor = True
+        Me.SimpleButton1.ButtonStyle = DevExpress.XtraEditors.Controls.BorderStyles.HotFlat
+        Me.SimpleButton1.Location = New System.Drawing.Point(5, 5)
+        Me.SimpleButton1.Name = "SimpleButton1"
+        Me.SimpleButton1.Size = New System.Drawing.Size(75, 27)
+        Me.SimpleButton1.TabIndex = 50
+        Me.SimpleButton1.Text = "View"
+        '
+        'SimpleButton5
+        '
+        Me.SimpleButton5.Appearance.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Bold)
+        Me.SimpleButton5.Appearance.ForeColor = System.Drawing.Color.Green
+        Me.SimpleButton5.Appearance.Options.UseFont = True
+        Me.SimpleButton5.Appearance.Options.UseForeColor = True
+        Me.SimpleButton5.ButtonStyle = DevExpress.XtraEditors.Controls.BorderStyles.HotFlat
+        Me.SimpleButton5.Location = New System.Drawing.Point(204, 5)
+        Me.SimpleButton5.Name = "SimpleButton5"
+        Me.SimpleButton5.Size = New System.Drawing.Size(75, 27)
+        Me.SimpleButton5.TabIndex = 49
+        Me.SimpleButton5.Text = "Close"
+        '
+        'SimpleButton3
+        '
+        Me.SimpleButton3.Appearance.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Bold)
+        Me.SimpleButton3.Appearance.ForeColor = System.Drawing.Color.Green
+        Me.SimpleButton3.Appearance.Options.UseFont = True
+        Me.SimpleButton3.Appearance.Options.UseForeColor = True
+        Me.SimpleButton3.ButtonStyle = DevExpress.XtraEditors.Controls.BorderStyles.HotFlat
+        Me.SimpleButton3.Location = New System.Drawing.Point(87, 5)
+        Me.SimpleButton3.Name = "SimpleButton3"
+        Me.SimpleButton3.Size = New System.Drawing.Size(110, 27)
+        Me.SimpleButton3.TabIndex = 48
+        Me.SimpleButton3.Text = "Export to Excel"
+        '
+        'Panel7
+        '
+        Me.Panel7.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Panel7.Controls.Add(Me.LabelControl2)
+        Me.Panel7.Controls.Add(Me.LabelControl1)
+        Me.Panel7.Controls.Add(Me.LabelControl80)
+        Me.Panel7.Controls.Add(Me.LabelControl81)
+        Me.Panel7.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Panel7.ForeColor = System.Drawing.Color.Black
+        Me.Panel7.Location = New System.Drawing.Point(0, 43)
+        Me.Panel7.Name = "Panel7"
+        Me.Panel7.Size = New System.Drawing.Size(153, 450)
+        Me.Panel7.TabIndex = 88
+        '
+        'LabelControl2
+        '
+        Me.LabelControl2.Appearance.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Bold)
+        Me.LabelControl2.Appearance.Options.UseFont = True
+        Me.LabelControl2.Location = New System.Drawing.Point(12, 115)
+        Me.LabelControl2.Name = "LabelControl2"
+        Me.LabelControl2.Size = New System.Drawing.Size(58, 13)
+        Me.LabelControl2.TabIndex = 10
+        Me.LabelControl2.Text = "MATERIAL"
+        '
+        'LabelControl1
+        '
+        Me.LabelControl1.Appearance.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Bold)
+        Me.LabelControl1.Appearance.Options.UseFont = True
+        Me.LabelControl1.Location = New System.Drawing.Point(12, 94)
+        Me.LabelControl1.Name = "LabelControl1"
+        Me.LabelControl1.Size = New System.Drawing.Size(94, 13)
+        Me.LabelControl1.TabIndex = 9
+        Me.LabelControl1.Text = "TYPE TRANSAKSI"
+        '
+        'LabelControl80
+        '
+        Me.LabelControl80.Appearance.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Bold)
+        Me.LabelControl80.Appearance.Options.UseFont = True
+        Me.LabelControl80.Location = New System.Drawing.Point(12, 73)
+        Me.LabelControl80.Name = "LabelControl80"
+        Me.LabelControl80.Size = New System.Drawing.Size(47, 13)
+        Me.LabelControl80.TabIndex = 8
+        Me.LabelControl80.Text = "TO DATE"
+        '
+        'LabelControl81
+        '
+        Me.LabelControl81.Appearance.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Bold)
+        Me.LabelControl81.Appearance.Options.UseFont = True
+        Me.LabelControl81.Location = New System.Drawing.Point(12, 52)
+        Me.LabelControl81.Name = "LabelControl81"
+        Me.LabelControl81.Size = New System.Drawing.Size(29, 13)
+        Me.LabelControl81.TabIndex = 7
+        Me.LabelControl81.Text = "DATE"
+        '
+        'BunifuGradientPanel2
+        '
+        Me.BunifuGradientPanel2.BackgroundImage = CType(resources.GetObject("BunifuGradientPanel2.BackgroundImage"), System.Drawing.Image)
+        Me.BunifuGradientPanel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.BunifuGradientPanel2.Controls.Add(Me.LabelControl89)
+        Me.BunifuGradientPanel2.Dock = System.Windows.Forms.DockStyle.Top
+        Me.BunifuGradientPanel2.GradientBottomLeft = System.Drawing.Color.DarkGreen
+        Me.BunifuGradientPanel2.GradientBottomRight = System.Drawing.Color.White
+        Me.BunifuGradientPanel2.GradientTopLeft = System.Drawing.Color.Green
+        Me.BunifuGradientPanel2.GradientTopRight = System.Drawing.Color.White
+        Me.BunifuGradientPanel2.Location = New System.Drawing.Point(0, 0)
+        Me.BunifuGradientPanel2.Name = "BunifuGradientPanel2"
+        Me.BunifuGradientPanel2.Quality = 10
+        Me.BunifuGradientPanel2.Size = New System.Drawing.Size(833, 43)
+        Me.BunifuGradientPanel2.TabIndex = 87
+        '
+        'ComboBoxEdit2
+        '
+        Me.ComboBoxEdit2.EnterMoveNextControl = True
+        Me.ComboBoxEdit2.Location = New System.Drawing.Point(159, 158)
+        Me.ComboBoxEdit2.Name = "ComboBoxEdit2"
+        Me.ComboBoxEdit2.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.ComboBoxEdit2.Size = New System.Drawing.Size(192, 20)
+        Me.ComboBoxEdit2.TabIndex = 97
+        '
+        'ComboBoxEdit1
+        '
+        Me.ComboBoxEdit1.EnterMoveNextControl = True
+        Me.ComboBoxEdit1.Location = New System.Drawing.Point(159, 136)
+        Me.ComboBoxEdit1.Name = "ComboBoxEdit1"
+        Me.ComboBoxEdit1.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.ComboBoxEdit1.Size = New System.Drawing.Size(192, 20)
+        Me.ComboBoxEdit1.TabIndex = 96
+        '
+        'DateEdit1
+        '
+        Me.DateEdit1.EditValue = Nothing
+        Me.DateEdit1.EnterMoveNextControl = True
+        Me.DateEdit1.Location = New System.Drawing.Point(159, 92)
+        Me.DateEdit1.Name = "DateEdit1"
+        Me.DateEdit1.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.DateEdit1.Properties.CalendarTimeProperties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.DateEdit1.Size = New System.Drawing.Size(192, 20)
+        Me.DateEdit1.TabIndex = 94
+        '
+        'DateEdit2
+        '
+        Me.DateEdit2.EditValue = Nothing
+        Me.DateEdit2.EnterMoveNextControl = True
+        Me.DateEdit2.Location = New System.Drawing.Point(159, 114)
+        Me.DateEdit2.Name = "DateEdit2"
+        Me.DateEdit2.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.DateEdit2.Properties.CalendarTimeProperties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.DateEdit2.Size = New System.Drawing.Size(192, 20)
+        Me.DateEdit2.TabIndex = 95
+        '
+        'FrmRptPenerimaan
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(833, 493)
+        Me.Controls.Add(Me.ComboBoxEdit2)
+        Me.Controls.Add(Me.ComboBoxEdit1)
+        Me.Controls.Add(Me.DateEdit1)
+        Me.Controls.Add(Me.DateEdit2)
+        Me.Controls.Add(Me.GridControl2)
+        Me.Controls.Add(Me.PanelControl6)
+        Me.Controls.Add(Me.Panel7)
+        Me.Controls.Add(Me.BunifuGradientPanel2)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "FrmRptPenerimaan"
+        Me.Text = "FrmRnumpang"
+        CType(Me.GridControl2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GridView2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PanelControl6, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PanelControl6.ResumeLayout(False)
+        Me.Panel7.ResumeLayout(False)
+        Me.Panel7.PerformLayout()
+        Me.BunifuGradientPanel2.ResumeLayout(False)
+        Me.BunifuGradientPanel2.PerformLayout()
+        CType(Me.ComboBoxEdit2.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ComboBoxEdit1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DateEdit1.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DateEdit1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DateEdit2.Properties.CalendarTimeProperties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DateEdit2.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GridColumn1 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridControl2 As DevExpress.XtraGrid.GridControl
+    Friend WithEvents GridView2 As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents LabelControl89 As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents PanelControl6 As DevExpress.XtraEditors.PanelControl
+    Friend WithEvents SimpleButton1 As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents SimpleButton5 As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents SimpleButton3 As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents Panel7 As Panel
+    Friend WithEvents BunifuGradientPanel2 As ns1.BunifuGradientPanel
+    Friend WithEvents ComboBoxEdit2 As DevExpress.XtraEditors.ComboBoxEdit
+    Friend WithEvents ComboBoxEdit1 As DevExpress.XtraEditors.ComboBoxEdit
+    Friend WithEvents DateEdit1 As DevExpress.XtraEditors.DateEdit
+    Friend WithEvents DateEdit2 As DevExpress.XtraEditors.DateEdit
+    Friend WithEvents LabelControl2 As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents LabelControl1 As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents LabelControl80 As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents LabelControl81 As DevExpress.XtraEditors.LabelControl
+End Class
